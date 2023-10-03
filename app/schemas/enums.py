cf8630e97c070ffd28eeff9b94fc4163fb1a5433
Ahmed Ashraf <ahmedabdou1410@gmail.com>
2023-10-04 02:06:08 +0300

    Create cipher enums & migration revision

---
+++ app/schemas/enums.py
@@ -0,0 +1,27 @@
+from enum import Enum, auto
+
+from typing_extensions import override
+
+
+class BaseEnum(str, Enum):
+    """
+    Base class for all enums
+    """
+
+    @override
+    def _generate_next_value_(name, start, count, last_values) -> str:  # type: ignore
+        """
+        Overrides the enum function that generates the next value
+        Uses the name as the automatic value, rather than an integer
+        See https://docs.python.org/3/library/enum.html#using-automatic-values for reference
+        """
+        return name
+
+    @classmethod
+    def as_dict(cls) -> dict:
+        return {e.name: e.value for e in cls}
+
+
+class CipherType(BaseEnum):
+    LOGIN = auto()
+    NOTE = auto()
