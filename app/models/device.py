973327d100afce67e438096db59afe6bf2c71bf0
Ahmed Ashraf <ahmedabdou1410@gmail.com>
2023-10-09 00:52:29 +0300

    Replace device_* with user_agent

--- app/models/device.py
+++ app/models/device.py
@@ -18,8 +18,7 @@ https://stackoverflow.com/questions/166132/maximum-length-of-the-textual-represe
 class Device(BaseModel):
     id: Mapped[uuid.UUID] = mapped_column(pg.UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     user_id: Mapped[uuid.UUID] = mapped_column(pg.UUID(as_uuid=True), ForeignKey("user.id"), index=True, nullable=False)
-    device_type: Mapped[str | None] = mapped_column(nullable=True)
-    device_os: Mapped[str | None] = mapped_column(nullable=True)
+    user_agent: Mapped[str] = mapped_column(String(length=350), nullable=False)
     regestered_at: Mapped[dt.datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
     last_login_ip: Mapped[IPvAnyAddress] = mapped_column(String(length=45), nullable=False)
     last_login_at: Mapped[dt.datetime] = mapped_column(DateTime(timezone=True), nullable=False)
--- app/schemas/device.py
+++ app/schemas/device.py
@@ -7,8 +7,17 @@ from app.schemas.base import BaseSchema
 class Device(BaseSchema):
     id: uuid.UUID
     user_id: uuid.UUID
-    device_type: str
-    device_os: str
+    user_agent: str
     regeistered_at: dt.datetime
     last_login_ip: dt.datetime
     last_login_at: dt.datetime
+
+
+class DeviceCreate(BaseSchema):
+    id: uuid.UUID
+    user_id: uuid.UUID
+    user_agent: str
+
+
+def user_agent_extract(ua: str) -> dict:
+    """Extracts device id, device type and os from user agent string."""
