565d96d8206461092068054170b392898f55afb8
Ahmed Ashraf <ahmedabdou1410@gmail.com>
2023-10-16 01:46:15 +0300

    Await redis connection on entrypoint script

--- app/entrypoint.py
+++ app/entrypoint.py
@@ -11,6 +11,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql import text
 from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
 
+from app.cache.client import AsyncRedisClient
 from app.db.session import AsyncSessionFactory
 
 logging.basicConfig(level=logging.INFO)
@@ -41,10 +42,32 @@ async def init_db_connection() -> None:
         raise e
 
 
+@retry(
+    stop=stop_after_attempt(MAX_TRIES),
+    wait=wait_fixed(WAIT_TIME),
+    before=before_log(logger, logging.INFO),
+    after=after_log(logger, logging.WARN),
+)
+async def init_redis_connection() -> None:
+    """Wait till redis connection is established"""
+    try:
+        async with timeout(TIMEOUT):
+            redis = AsyncRedisClient()
+            await redis.redis.ping()
+    except Exception as e:
+        logger.error("--- Connection to Redis failed ---")
+        logger.log(logging.ERROR, e)
+        raise e
+
+
 async def main() -> None:
     logger.info("--- Connecting TO POSTGRESQL DB ---")
     await init_db_connection()
     logger.info("--- Connection TO POSTGRESQL DB ESTABLISHED ---")
+
+    logger.info("--- Connecting TO REDIS ---")
+    await init_redis_connection()
+    logger.info("--- Connection TO REDIS ESTABLISHED ---")
 
 
 if __name__ == "__main__":
