3a1eb122beaff9d14f39f36216a90dc56ae19af7
Ahmed Ashraf <root@xps>
2023-11-10 02:36:18 +0300

    Fix mock worker job type error

--- app/utils/mocks.py
+++ app/utils/mocks.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 import rq
 
 from app import schemas
@@ -7,10 +9,10 @@ from app.schemas import enums
 def mock_worker_job(**kwargs) -> schemas.WorkerJob:
     return schemas.WorkerJob(
         id="1",
-        created_at="2021-01-01 00:00:00",
-        started_at="2021-01-01 00:00:00",
-        ended_at="2021-01-01 00:00:00",
-        enqueued_at="2021-01-01 00:00:00",
+        created_at=dt.datetime.now(dt.UTC),
+        started_at=dt.datetime.now(dt.UTC),
+        enqueued_at=dt.datetime.now(dt.UTC),
+        ended_at=dt.datetime.now(dt.UTC) + dt.timedelta(seconds=1),
         result="result",
         result_ttl=1,
         status=rq.job.JobStatus.QUEUED,
