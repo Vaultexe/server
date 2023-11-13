db5c28b62d4be04544605bc02825ed1916f00dba
Ahmed Ashraf <root@xps>
2023-11-13 18:45:06 +0300

    Create refresh endpoint

--- app/api/deps/auth.py
+++ app/api/deps/auth.py
@@ -1,4 +1,3 @@
-import datetime as dt
 from typing import Annotated
 
 from fastapi import Cookie, Depends, Request
@@ -14,7 +13,6 @@ from app.schemas.token import AccessTokenClaim, OTPTokenClaim, RefreshTokenClaim
 from app.utils.exceptions import (
     AuthenticationException,
     AuthorizationException,
-    TokenExpiredException,
 )
 
 
@@ -36,6 +34,7 @@ ReqUserAgentDep = Annotated[str, Depends(get_user_agent)]
 DeviceIDCookieDep = Annotated[str | None, Cookie(alias=CookieKey.DEVICE_ID)]
 OTPTokenCookieDep = Annotated[str | None, Cookie(alias=CookieKey.OTP_TOKEN)]
 AccessTokenCookieDep = Annotated[str | None, Cookie(alias=CookieKey.ACCESS_TOKEN)]
+RefreshTokenCookieDep = Annotated[str | None, Cookie(alias=CookieKey.REFRESH_TOKEN)]
 
 
 async def get_curr_device(
@@ -44,7 +43,7 @@ async def get_curr_device(
 ) -> models.Device | None:
     """
     Returns the device making the request
-    if it was registered in the system
+    if it was registered in the system (verified or not)
     else None
     """
     if device_id:
@@ -66,30 +65,44 @@ async def get_curr_verified_device(
     return None
 
 
+ReqDeviceDep = Annotated[models.Device | None, Depends(get_curr_device)]
+ReqVerifiedDeviceDep = Annotated[models.Device | None, Depends(get_curr_verified_device)]
+
+
 async def get_current_user(
     rc: AsyncRedisClientDep,
     db: DbDep,
     req_ip: ReqIpDep,
-    device_id: DeviceIDCookieDep = None,
+    req_device_id: DeviceIDCookieDep = None,
     token: AccessTokenCookieDep = None,
 ) -> models.User:
     """
     Returns the user making the request
     with the given access token
     if it is still valid
+    and with a verified device
     """
     if not token:
+        raise AuthenticationException
+
+    is_verified_device = await repo.device.is_verified(db, id=req_device_id)
+
+    if not is_verified_device:
         raise AuthenticationException
 
     at_claim = AccessTokenClaim.from_encoded(token)
 
     rt_claim = await cache.repo.get_token(
         rc=rc,
-        key=str((at_claim.sub, device_id)),
+        key=str((at_claim.sub, req_device_id)),
         token_cls=RefreshTokenClaim,
     )
 
-    if rt_claim is None or at_claim.jti != rt_claim.jti or str(req_ip) != str(rt_claim.ip):
+    if (
+        rt_claim is None \
+        or at_claim.jti != rt_claim.jti \
+        or str(req_ip) != str(rt_claim.ip)
+    ):  # fmt: off
         raise AuthenticationException
 
     user = await repo.user.get(db, id=at_claim.sub)
@@ -112,6 +125,58 @@ async def get_current_admin(
     return user
 
 
+async def get_refresh_user(
+    db: DbDep,
+    req_ip: ReqIpDep,
+    rc: AsyncRedisClientDep,
+    req_device_id: DeviceIDCookieDep = None,
+    access_token: AccessTokenCookieDep = None,
+    refresh_token: RefreshTokenCookieDep = None,
+) -> models.User:
+    """
+    Returns the user making the request
+    with the given refresh and access tokens
+    if refresh token is still valid
+    but access token is expired
+    """
+    if not refresh_token or not access_token:
+        raise AuthenticationException
+
+    is_verified_device = await repo.device.is_verified(db, id=req_device_id)
+
+    if not is_verified_device:
+        raise AuthenticationException
+
+    rt_claim = RefreshTokenClaim.from_encoded(refresh_token)
+    at_claim = AccessTokenClaim.from_encoded(access_token, allow_expired=True)
+
+    if (
+        at_claim.jti != rt_claim.jti \
+        or at_claim.sub != rt_claim.sub
+    ):  # fmt: off
+        raise AuthenticationException
+
+    validator_rt = await cache.repo.get_token(
+        rc=rc,
+        key=str((rt_claim.sub, req_device_id)),
+        token_cls=RefreshTokenClaim,
+    )
+
+    if (
+        validator_rt is None \
+        or rt_claim.jti != validator_rt.jti \
+        or str(req_ip) != str(rt_claim.ip)
+    ):  # fmt: off
+        raise AuthenticationException
+
+    user = await repo.user.get(db, id=rt_claim.sub)
+
+    if not user or not user.is_active:
+        raise AuthenticationException
+
+    return user
+
+
 async def get_otp_user(
     db: DbDep,
     token: OTPTokenCookieDep = None,
@@ -126,9 +191,6 @@ async def get_otp_user(
 
     otp_claim = OTPTokenClaim.from_encoded(token)
 
-    if otp_claim.exp < dt.datetime.now(dt.UTC):
-        raise TokenExpiredException
-
     user = await repo.user.get(db, id=str(otp_claim.sub))
 
     if not user:
@@ -137,9 +199,8 @@ async def get_otp_user(
     return user
 
 
-ReqDeviceDep = Annotated[models.Device | None, Depends(get_curr_device)]
-ReqVerifiedDeviceDep = Annotated[models.Device | None, Depends(get_curr_verified_device)]
 OAuth2PasswordRequestFormDep = Annotated[OAuth2PasswordRequestForm, Depends()]
 OTPUserDep = Annotated[models.User, Depends(get_otp_user)]
+RefreshUserDep = Annotated[models.User, Depends(get_refresh_user)]
 UserDep = Annotated[models.User, Depends(get_current_user)]
 AdminDep = Annotated[models.User, Depends(get_current_admin)]
