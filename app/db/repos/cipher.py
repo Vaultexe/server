ac842554d97b41e25f0c75358b3b6cf78b328bbf
Ahmed Ashraf <root@xps>
2023-12-04 11:47:53 +0300

    Fix comparison to None in cipher.py

--- app/db/repos/cipher.py
+++ app/db/repos/cipher.py
@@ -70,7 +70,7 @@ class CipherRepo(BaseRepo[models.Cipher, schemas.CipherCreate]):
         *,
         user_id: uuid.UUID,
     ) -> list[models.Cipher]:
-        filters = sa.and_(self.model.user_id == user_id, self.model.deleted_at is None)  # type: ignore
+        filters = sa.and_(self.model.user_id == user_id, self.model.deleted_at == None)
         return await super()._get_all(db, filter=filters)
 
 
