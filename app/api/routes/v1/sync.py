39cf63b657b83ec4bf85e576df377b9b87629e16
Ahmed Ashraf <root@xps>
2023-12-15 17:01:27 +0300

    Remove unused code for vault syncing

--- app/api/routes/v1/sync.py
+++ app/api/routes/v1/sync.py
@@ -1,12 +1,7 @@
-import uuid
-from collections.abc import AsyncIterator
-
 from fastapi import APIRouter
-from sse_starlette import EventSourceResponse, ServerSentEvent
+from sse_starlette import EventSourceResponse
 
 from app.api.deps import AsyncRedisClientDep, UserDep
-from app.cache.client import AsyncRedisClient
-from app.cache.key_gen import KeyGen
 from app.events import SyncData, listen
 
 router = APIRouter()
@@ -19,14 +14,3 @@ async def sse(
 ) -> EventSourceResponse:
     """Server sent events for vault syncing"""
     return EventSourceResponse(listen(rc, user_id=user.id))
-
-
-async def listen_to_updates(
-    user_id: uuid.UUID | str,
-    rc: AsyncRedisClient,
-) -> AsyncIterator:
-    async with rc.redis.pubsub() as pubsub:
-        await pubsub.subscribe(KeyGen.USER_SYNC_VAULT_PUBSUB_CHANNEL(user_id))
-        async for message in pubsub.listen():
-            if message["type"] == "message":
-                yield ServerSentEvent(SyncData.model_validate_json(message["data"]).model_dump())
