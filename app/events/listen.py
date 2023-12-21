50d5a2b7df94fdd9e6ded748d4cbca6f9bf727ec
Ahmed Ashraf <root@xps>
2023-12-21 13:44:42 +0300

    Add pubsub settings to ignore subscribe messages

--- app/events/listen.py
+++ app/events/listen.py
@@ -4,6 +4,9 @@ from collections.abc import AsyncIterator
 from app.cache.client import AsyncRedisClient
 from app.cache.key_gen import KeyGen
 
+__pubsub_settings__ = {
+    "ignore_subscribe_messages": True,
+}
 
 async def listen(
     rc: AsyncRedisClient,
@@ -11,7 +14,7 @@ async def listen(
     user_id: uuid.UUID,
 ) -> AsyncIterator:
     """Listen for user vault changes"""
-    async with rc.redis.pubsub() as pubsub:
+    async with rc.redis.pubsub(**__pubsub_settings__) as pubsub:
         await pubsub.subscribe(KeyGen.USER_SYNC_VAULT_PUBSUB_CHANNEL(user_id))
         async for message in pubsub.listen():
             if message["type"] == "message":
