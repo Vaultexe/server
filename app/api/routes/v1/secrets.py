ab228dd02ef2c153fd6a752eb2c717572f8f33af
ahmedabdou14 <root@xps>
2024-03-15 00:29:51 +0300

    Add apis

--- app/api/routes/v1/secrets.py
+++ app/api/routes/v1/secrets.py
@@ -25,6 +25,15 @@ async def get_secrets(
     return [schemas.Cipher.model_validate(cipher) for cipher in ciphers]
 
 
+@router.get("/deleted")
+async def get_deleted_secrets(
+    db: DbDep,
+    user: UserDep,
+) -> list[schemas.Cipher]:
+    ciphers = await repo.cipher._get_all_deleted(db, user_id=user.id)
+    return [schemas.Cipher.model_validate(cipher) for cipher in ciphers]
+
+
 @router.post("/")
 async def create_secret(
     db: DbDep,
@@ -86,8 +95,34 @@ async def update_secret(
     return secret
 
 
+@router.put("/restore/{cipher_id}")
+async def restore_secret(
+    db: DbDep,
+    user: UserDep,
+    rc: AsyncRedisClientDep,
+    cipher_id: Annotated[uuid.UUID, Path(...)],
+) -> schemas.Cipher:
+    """
+    ## Restore soft deleted secret
+
+    ## Sync event
+    Syncs user vault by notifying all
+    connected devices via redis pubsub
+    with a restore event
+    """
+    cipher = await repo.cipher.restore(db, id=cipher_id)
+    if not cipher:
+        raise EntityNotFoundException("Secret")
+    await db.commit()
+    await db.refresh(cipher)
+
+    secret = schemas.Cipher.model_validate(cipher)
+    await notify(rc, user_id=user.id, data=secret, action=Op.RESTORE)
+    return secret
+
+
 @router.delete("/{cipher_id}")
-async def delete_secret(
+async def soft_delete_secret(
     db: DbDep,
     user: UserDep,
     rc: AsyncRedisClientDep,
@@ -110,3 +145,24 @@ async def delete_secret(
     secret = schemas.Cipher.model_validate(cipher)
     await notify(rc, user_id=user.id, data=secret, action=Op.SOFT_DELETE)
     return secret
+
+@router.delete("/{cipher_id}/permanent")
+async def permanently_delete_secret(
+    db: DbDep,
+    user: UserDep,
+    rc: AsyncRedisClientDep,
+    cipher_id: Annotated[uuid.UUID, Path(...)],
+) -> None:
+    """
+    ## Permanently delete secret
+
+    ## Sync event
+    Syncs user vault by notifying all
+    connected devices via redis pubsub
+    with a delete event
+    """
+    has_deleted = await repo.cipher.permanent_delete(db, id=cipher_id)
+    if not has_deleted:
+        raise EntityNotFoundException("Secret")
+    await db.commit()
+    await notify(rc, user_id=user.id, data=cipher_id, action=Op.DELETE)
